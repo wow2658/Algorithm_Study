223eff3785e094613e8bd7c5d6fbbfc26b5dd430
Vp_pDoochi <wow2658@naver.com>
2024-04-25 13:27:36 +0900

    fail: 1780번(종이의 개수)-01

---
+++ 백준/바킹독/Recursion/05_1780번(종이의 개수)/01-2024-04-25-1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N;
+int paper[2200][2200];                      //3^7 = 2187
+int cnt[3];                                 //-1, 0, 1로 채워진 종이 갯수
+
+bool check(int x, int y, int n)             // main 첫스타트 00n은 n이 엉터리라 무쓸모
+{
+    for (int i = x; i < x + n; i++)
+        for (int j = y; j < y + n; j++)
+            if (paper[x][y] != paper[i][j])
+                return false;
+    return true;                            // 재귀로 인해 들어온 solve부터 진짜 유의미
+}
+void solve(int x, int y, int z)
+{
+    if (check(x, y, z))
+    {
+        cnt[paper[x][y] + 1] += 1;          // 종이->index, -1->0, 0->1, 1->2
+        return;                             // void 탈출
+    }
+
+    int n = z / 3;                          // 종이를 같은 크기의 종이 9개로 자르자고 했으니까
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            solve(x + i * n, y + j * n, n); // 0 3 6 부분 정복하겠다.
+}
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            cin >> paper[i][j];             // 정석대로 paper 2차원 보드 초기화
+    solve(0, 0, N);                         // 스타팅
+    for (int i = 0; i < 3; i++) cout << cnt[i] << "\n";
+}
---
+++ 백준/바킹독/Recursion/05_1780번(종이의 개수)/README.md
@@ -0,0 +1,1 @@
+﻿[![image](https://github.com/wow2658/CodingTest/assets/34699039/50206729-fbfd-4921-ad08-2ce0eac7d9d3)](https://www.acmicpc.net/problem/1780)
