928a0314f7a4bb1525ff5e9535d5c33c29a0da53
Vp_pDoochi <wow2658@naver.com>
2024-04-26 11:47:42 +0900

    success: 1780번(종이의 개수)-02
    체크부분 return;

---
+++ 백준/바킹독/Recursion/05_1780번(종이의 개수)/02+2024-04-25-1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n;
+int paper[2200][2200];
+int cnt[3];
+
+bool chk(int x, int y, int n)
+{
+	for (int r = x; r < x + n; r++)
+	{
+		for (int c = y; c < y + n; c++)
+		{
+			if (paper[x][y] != paper[r][c]) return false;
+		}
+	}
+	return true;
+}
+
+void solve(int x, int y, int z)
+{
+	if (chk(x, y, z))
+	{
+		cnt[paper[x][y] + 1]++;
+		return;					// 디버깅으로 잡아냄. 안하면 무한루프 실수
+	}
+
+	int sn = z / 3;
+	for (int r = 0; r < 3; r++)
+	{
+		for (int c = 0; c < 3; c++)
+		{
+			solve(x + r * sn, y + c * sn, sn);
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int r = 0; r < n; r++)
+	{
+		for (int c = 0; c < n; c++)
+		{
+			cin >> paper[r][c];
+		}
+	}
+	solve(0, 0, n);
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << cnt[i] << "\n";
+	}
+
+}
