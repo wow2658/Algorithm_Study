a8312a83f871cfa4e98cc4543c0dee380370fc8f
이건탁 <wow2658@naver.com>
2024-06-07 23:27:18 +0900

    success: 9095번(1, 2, 3 더하기)-01

---
+++ 백준/바킹독/DP/02_9095번(1, 2, 3 더하기)/01_2024-06-07-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int d[12];
+int t, n;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 4;
+	cin >> t;
+	for (int i = 4; i < 11; ++i)
+	{
+		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+	}
+
+	while (t--)
+	{
+		cin >> n;
+
+		cout << d[n] << "\n";
+	}
+
+}
---
+++ 백준/바킹독/DP/02_9095번(1, 2, 3 더하기)/README.md
@@ -0,0 +1,1 @@
+﻿[![image](https://github.com/wow2658/CodingTest/assets/34699039/c20cf2f1-5fa4-4f12-b764-ae75ac56ac8b)](https://www.acmicpc.net/problem/9095)
