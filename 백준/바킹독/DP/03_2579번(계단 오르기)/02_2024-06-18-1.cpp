9bb4f084868908647425d34391eb5cc61b1bd12c
이건탁 <wow2658@naver.com>
2024-06-18 21:17:51 +0900

    fail: 2579번(계단 오르기 별해 1)-02
    1. d[i-1]도 안밟으면 d[i-2]에서 한번에 3계단을 도약해야해서 근본 규칙 위배
    2. positive max = total - negative min

---
+++ 백준/바킹독/DP/03_2579번(계단 오르기)/02_2024-06-18-1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int s[305];
+int n;
+int d[305];
+
+int main(void) 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    int tot = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> s[i];
+        tot += s[i];
+    }
+
+    if (n <= 2) 
+    {
+        cout << tot;
+        return 0;
+    }
+
+    d[1] = s[1];
+    d[2] = s[2];
+    d[3] = s[3];
+
+    for (int i = 4; i <= n - 1; i++) 
+        d[i] = min(d[i - 2], d[i - 3]) + s[i];  // d[i-1]도 안밟으면 d[i-2]에서 한번에 3계단을 도약해야해서 근본 규칙 위배 
+    cout << tot - min(d[n - 1], d[n - 2]);      // positive max = total - negative min
+}
