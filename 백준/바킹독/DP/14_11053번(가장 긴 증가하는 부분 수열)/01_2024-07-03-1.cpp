24e7837aa1ff6e11facb885bdb6cede68d3ba47c
Vp_pDoochi <wow2658@naver.com>
2024-07-03 22:44:58 +0900

    fail: 11053번(가장 긴 증가하는 부분 수열)-01

---
+++ 백준/바킹독/DP/14_11053번(가장 긴 증가하는 부분 수열)/01_2024-07-03-1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+int a[1010], d[1010];
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a[i];
+		//d[i] = a[i];
+	}
+
+	fill(d, d + n, 1); // d 배열을 1로 초기화, 각 원소 자체만으로 부분 수열을 구성한다고 가정
+
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < i; ++j)
+			if (a[j] < a[i])
+				//d[i] = max(d[i], d[j] + a[i]); 
+				d[i] = max(d[i], d[j] + 1); // 누적합이 아니라 누석개수
+
+	cout << *max_element(d, d + n);
+}
---
+++ 백준/바킹독/DP/14_11053번(가장 긴 증가하는 부분 수열)/README.md
@@ -0,0 +1,1 @@
+﻿[![image](https://github.com/wow2658/CodingTest/assets/34699039/76279635-5905-4c6f-8fd5-e68e3e0e7a70)](https://www.acmicpc.net/problem/11053)
