822072c574880ffd6ec5b4a1fba48561113b69e1
이건탁 <wow2658@naver.com>
2024-07-03 10:24:25 +0900

    fail: 11055번(가장 큰 증가하는 부분 수열)-01

---
+++ 백준/바킹독/DP/13_11055번(가장 큰 증가하는 부분 수열)/01_2024-07-03-1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+int a[1010], d[1010];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+        d[i] = a[i];
+    }
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < i; ++j)
+            if (a[j] < a[i])                    // 증가하는 수열을 만드는것이 조건이었기 때문이다.
+                d[i] = max(d[i], d[j] + a[i]);  // d[j]: 현재까지 구한 부분 수열 중에서, 
+    //       마지막 원소가 a[j]인 증가하는 부분 수열의 최대 합.
+    // 계속 변화하는, 즉 여러 d[j]중에 가장 가능성있어보이는 친구에게 투자하겠다는 느낌
+
+    cout << *max_element(d, d + n);
+}
---
+++ 백준/바킹독/DP/13_11055번(가장 큰 증가하는 부분 수열)/README.md
@@ -0,0 +1,1 @@
+﻿[![image](https://github.com/wow2658/CodingTest/assets/34699039/5d02df03-4fc9-414d-90ee-2720d6b956e7)](https://www.acmicpc.net/problem/11055)
