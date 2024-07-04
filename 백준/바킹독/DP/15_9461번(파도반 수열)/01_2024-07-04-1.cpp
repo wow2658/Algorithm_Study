6194e6fc11ed5132ce820c8cab93204221c9f092
이건탁 <wow2658@naver.com>
2024-07-04 20:05:45 +0900

    fail: 9461번(파도반 수열)-01

---
+++ 백준/바킹독/DP/15_9461번(파도반 수열)/01_2024-07-04-1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int t, n;
+long long d[105];
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	d[1] = 1; d[2] = 1; d[3] = 1;
+	for (int i = 4; i < 101; i++)
+	{
+		d[i] = d[i - 2] + d[i - 3];
+	}
+
+	cin >> t;
+
+	for (int i = 0; i < t; ++i)
+	{
+		cin >> n;
+		cout << d[n] << "\n";
+
+	}
+}
---
+++ 백준/바킹독/DP/15_9461번(파도반 수열)/README.md
@@ -0,0 +1,1 @@
+﻿[![image](https://github.com/wow2658/CodingTest/assets/34699039/42797eb1-aced-4ce6-a53d-abf5d86d54f7)](https://www.acmicpc.net/problem/9461)
