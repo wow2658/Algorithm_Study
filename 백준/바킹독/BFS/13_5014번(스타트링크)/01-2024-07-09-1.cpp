d47b0bfb74c604ced3e988444e312a49d7db543b
이건탁 <wow2658@naver.com>
2024-07-09 10:56:17 +0900

    fail: 5014번(스타트링크)-01
    1층부터 f층 포함까지, want -1
    >> u >> d, cur + u,cur - d
    nxt > f || nxt <= 0

---
+++ 백준/바킹독/BFS/13_5014번(스타트링크)/01-2024-07-09-1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int f, s, g, u, d;
+int dist[1000002];
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> f >> s >> g >> u >> d;
+	// 1층부터 f층 포함까지
+	fill(dist + 1, dist + f + 1, -1);
+
+	queue<int> q;
+	dist[s] = 0;
+	q.push(s);
+	while (!q.empty())
+	{
+		int cur = q.front(); q.pop();
+		// U버튼은 위로 U층을 가고 D버튼은 아래로 D층. 즉, 껑충껑충 다닌다.
+		for (auto& nxt : { cur + u,cur - d })
+		{
+			// f층 포함, 건물은 1층부터 시작
+			if (nxt > f || nxt <= 0 || dist[nxt] != -1) continue;
+			dist[nxt] = dist[cur] + 1;
+			q.push(nxt);
+		}
+	}
+
+	if (dist[g] == -1)
+		cout << "use the stairs";
+	else cout << dist[g];
+	return 0;
+}
---
+++ 백준/바킹독/BFS/13_5014번(스타트링크)/README.md
@@ -0,0 +1,1 @@
+[![image](https://github.com/wow2658/CodingTest/assets/34699039/6a39b555-9584-4f64-a3a8-2ab02752a710)](https://www.acmicpc.net/problem/5014)
