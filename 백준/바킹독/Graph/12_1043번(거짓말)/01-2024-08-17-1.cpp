47638823ef5ebb8ce8b8bba4c3db42db30819d69
이건탁 <wow2658@naver.com>
2024-08-17 23:58:44 +0900

    Fix: 1043번(거짓말)-01

--- 백준/바킹독/Graph/12_1043번(거짓말)/01-2024-08-17-1.cpp
+++ 백준/바킹독/Graph/12_1043번(거짓말)/01-2024-08-17-1.cpp
@@ -1,96 +1,89 @@
 #include <iostream>
+#include <unordered_set>
 #include <queue>
 
 using namespace std;
 
-// 구슬의 개수와 저울에 올려 본 쌍의 개수
-int n, m;
+// 부모 노드를 저장하는 배열 (유니온 파인드를 위해 사용)
+int parent[51];
 
-// 구슬이 무거운 방향과 가벼운 방향을 저장하는 인접 리스트
-vector<int> hv[101], li[101];
-
-// 방문 여부를 체크하는 배열
-bool vis[101];
-
-// 구슬이 무게 순서의 중간에 있을 수 없는지 판별하는 함수
-//bool solve(int st, vector<int>(&adj)[]) // 컴파일 오류
-bool solve(int st, vector<int>(adj)[])
-{
-    // 방문 배열을 초기화하여 모든 정점을 미방문 상태로 설정
-    fill(vis, vis + n + 1, 0);
-
-    // BFS를 위한 큐 선언
-    queue<int> q;
-
-    // 시작 정점을 큐에 추가하고 방문 표시
-    q.push(st);
-    vis[st] = 1;
-
-    // 현재 구슬보다 무거운/가벼운 구슬의 개수
-    int cnt = 0;
-
-    // BFS 수행
-    while (!q.empty())
-    {
-        // 큐에서 현재 정점을 꺼냄
-        int cur = q.front(); q.pop();
-
-        // 현재 정점과 연결된 모든 정점을 순회
-        for (int nxt : adj[cur])
-        {
-            // 이미 방문한 정점이면 건너뜀
-            if (vis[nxt])
-            {
-                continue;
-            }
-
-            // 다음 정점을 큐에 추가하고 방문 표시
-            q.push(nxt);
-            vis[nxt] = 1;
-
-            // 카운터 증가
-            cnt++;
-        }
+// find 함수: 주어진 노드의 루트 노드를 찾음
+int find(int x) {
+    if (parent[x] == x) {
+        return x; // 자기 자신이 루트라면 자기 자신을 반환
     }
-
-    // 현재 정점보다 무거운 구슬 또는 가벼운 구슬의 수가
-    // 전체 구슬의 절반 이상이면 중간에 있을 수 없음
-    return cnt >= (n + 1) / 2;
+    else {
+        return parent[x] = find(parent[x]); // 재귀적으로 루트 노드를 찾고 경로 압축
+    }
 }
 
-int main(void)
-{
+// union 함수: 두 집합을 합침 (노드 x와 y의 루트를 합침)
+void union_set(int x, int y) {
+    int rootX = find(x); // x의 루트 노드 찾기
+    int rootY = find(y); // y의 루트 노드 찾기
+    if (rootX != rootY) {
+        parent[rootY] = rootX; // 두 루트가 다르다면, y의 루트를 x의 루트로 변경
+    }
+}
+
+int main() {
     // 빠른 입출력을 위한 설정
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // 구슬의 개수와 저울에 올려 본 쌍의 개수를 입력받음
-    cin >> n >> m;
+    // 사람의 수 N과 파티의 수 M 입력받기
+    int N, M;
+    cin >> N >> M;
 
-    // 저울에 올려 본 쌍의 개수 만큼 반복
-    while (m--)
-    {
-        // 구슬의 무게 비교 결과 입력받음
-        int u, v;
-        cin >> u >> v;
-
-        // 구슬 u가 구슬 v보다 무겁다는 정보 저장
-        hv[u].push_back(v);
-
-        // 구슬 v가 구슬 u보다 가볍다는 정보 저장
-        li[v].push_back(u);
+    // 각 사람의 초기 루트는 자기 자신으로 설정
+    for (int i = 1; i <= N; i++) {
+        parent[i] = i;
     }
 
-    // 무게가 중간이 될 수 없는 구슬의 개수
-    int ans = 0;
-
-    // 각 구슬에 대해 solve 함수 호출
-    for (int i = 1; i <= n; i++)
-    {
-        // 구슬 i가 무게 중간 순서에 있을 수 없는 경우 증가
-        ans += (solve(i, hv) || solve(i, li));
+    // 진실을 아는 사람 수와 그 사람들의 번호를 입력받기
+    int numOfTruth;
+    cin >> numOfTruth;
+    vector<int> truth(numOfTruth); // 진실을 아는 사람들의 목록
+    for (int i = 0; i < numOfTruth; i++) {
+        cin >> truth[i];
     }
 
-    // 결과 출력
-    cout << ans;
+    vector<vector<int>> parties(M); // 각 파티에 참석하는 사람들 저장
+    for (int i = 0; i < M; i++) {
+        int num; // 파티에 참석하는 사람의 수
+        cin >> num;
+        parties[i].resize(num); // 파티에 참석하는 사람들의 수에 맞게 크기 조정
+        for (int j = 0; j < num; j++) {
+            cin >> parties[i][j];
+        }
+
+        // 파티에 참석하는 모든 사람들을 같은 집합으로 묶음
+        for (int j = 1; j < num; j++) {
+            union_set(parties[i][0], parties[i][j]);
+        }
+    }
+
+    // 진실을 아는 모든 사람의 루트를 찾아서 저장
+    unordered_set<int> truth_roots;
+    for (int i = 0; i < numOfTruth; i++) {
+        truth_roots.insert(find(truth[i])); // 진실을 아는 사람들의 루트를 집합에 추가
+    }
+
+    int count = 0; // 거짓말을 할 수 있는 파티 수를 세는 변수
+
+    // 각 파티를 확인하며 거짓말을 할 수 있는지 체크
+    for (int i = 0; i < M; i++) {
+        bool canLie = true; // 거짓말을 할 수 있다고 가정
+        for (int j = 0; j < parties[i].size(); j++) {
+            if (truth_roots.count(find(parties[i][j]))) {
+                canLie = false; // 파티 참석자 중 루트가 진실을 아는 사람과 같은 경우 거짓말 불가
+                break;
+            }
+        }
+        if (canLie) {
+            count++; // 거짓말을 할 수 있는 파티 수 증가
+        }
+    }
+
+    cout << count; // 결과 출력
 }
